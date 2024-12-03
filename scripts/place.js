23cdec6126285b852859f80560e70a6447df24a8
Daberechi Levi Nwachukwu <daberelevi@gmail.com>
2024-12-03 06:51:12 -0800

    Update place.js

--- scripts/place.js
+++ scripts/place.js
@@ -8,19 +8,13 @@ const windSpeed = 10; // in km/h
 
 // Function to calculate windchill
 function calculateWindChill(temp, wind) {
-  if ((temp <= 10 && wind > 4.8) || (temp <= 50 && wind > 3)) {
-    // Wind chill formula for metric/imperial
-    return (
-      13.12 +
-      0.6215 * temp -
-      11.37 * Math.pow(wind, 0.16) +
-      0.3965 * temp * Math.pow(wind, 0.16)
-    ).toFixed(2);
-  } else {
-    return "N/A";
-  }
+    if ((temp <= 10 && wind > 4.8) || (temp <= 50 && wind > 3)) {
+        // Wind chill formula for metric/imperial
+        return (13.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0.16)).toFixed(2);
+    } else {
+        return "N/A";
+    }
 }
 
 // Display wind chill value
-document.getElementById("windchill").textContent =
-  "Wind Chill: " + calculateWindChill(temperature, windSpeed) + "°C";
+document.getElementById("windchill").textContent = "Wind Chill: " + calculateWindChill(temperature, windSpeed) + "°C";
