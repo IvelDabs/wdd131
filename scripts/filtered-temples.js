0148eea94668b837e5d1d638d4bf64933115587f
Daberechi Levi Nwachukwu <daberelevi@gmail.com>
2024-12-07 06:43:45 +0100

    Update filtered-temples.js

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -91,7 +91,7 @@ function renderTemples(templesArray) {
     const card = document.createElement("div");
     card.classList.add("temple-card");
     card.innerHTML = `
-            <h2>${temple.templeName}</h2>
+            <h3>${temple.templeName}</h3>
             <p>${temple.location}</p>
             <p>Dedicated: ${temple.dedicated}</p>
             <p>Area: ${temple.area} sq ft</p>
